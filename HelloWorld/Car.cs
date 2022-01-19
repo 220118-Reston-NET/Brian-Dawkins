03b77a35715eca612feeb432a75348864c80bfbb
Brian Dawkins <brian.dawkins@reature.net>
2022-01-19 14:01:18 -0500

    First Init

--- HelloWorld/Car.cs
+++ HelloWorld/Car.cs
@@ -7,5 +7,31 @@ namespace CarFunction
 public class Car
 {
 
+//Field 
+//It is used to store information or define the current state of the object when you first make it
+// "_" is good practice to use in coding to make it easier
+private string _color = "Blue"; 
+private string _owner;
+private int _fuel;
+private int _gallonPerMile;
+
+//Property
+//They are in PascalCase ThisIsHowPascalCaseWillMakeASentence
+public string Color
+{
+    //get keyword in a property is how you can make one of your private fields reasonable 
+    get {return "the color is " + _color;}
+    
+    //set keyword in a property is how you can make one your private fields writeable 
+    set { _color = value;}
+}
+
+public string Owner
+{
+    get { return _owner; }
+    set { _owner = value; }
+}
+
+
 }
 }
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CarFunction;
+
+//This will print whatever string you put in the parentheses in the terminal 
+Console.WriteLine("Hello, World!");
+
+//NameofClass NameofVariable = new NameofClass();
+Car car1 = new Car();
+Console.WriteLine(car1.Color);
+car1.Color = "Red";
+Console.WriteLine(car1.Color);
