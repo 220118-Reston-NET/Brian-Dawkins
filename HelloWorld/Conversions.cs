8eda0ae63b63395bc8de1a0e6b1a8994409c1a0a
Brian Dawkins <brian.dawkins@reature.net>
2022-01-20 10:56:32 -0500

    First Init

--- HelloWorld/Conversions.cs
+++ HelloWorld/Conversions.cs
@@ -12,7 +12,26 @@ namespace ConversionFunction
         //static methods will make using that method without creating an object
         public static void ConversionMain()
         {
+            //Implicit - just means something is done automatically (mostly it is compiler)
+            //Explicit - just menas you have to write something to do it (essentially tell the compiler to do it anyway)
 
+            Console.WriteLine("===Conversion Demo===");
+            int x = 10;
+            double y = 85.29;
+            string str = "Hello World";
+
+            //Implicit conversion
+            //The general rule is if you are going from a datatype tp another datatype wihtout losing information
+            double anotherDouble = x;
+            Console.WriteLine(anotherDouble); 
+
+            //Explicit conversion (ex: casting)
+            //The general rule is if you are going to lose information if you are trying to convert
+            //The compiler wont do it for you for obvious reasons
+            int anotherInt = (int)y;
+            Console.WriteLine(anotherInt);
+
+            //Another way of explicit conversion 
         }
 
         public int GiveMeNumber()
