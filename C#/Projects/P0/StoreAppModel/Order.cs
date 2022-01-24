76cda4b4500189e5d152c56295ec9172e6946fb4
Brian Dawkins <brian.dawkins@reature.net>
2022-01-24 16:52:58 -0500

    Model changes

--- C#/Projects/P0/StoreAppModel/Order.cs
+++ C#/Projects/P0/StoreAppModel/Order.cs
@@ -5,5 +5,20 @@ namespace OrderModel
         public string LineItems { get; set; }
         public string Location { get; set ;}
         public double Total {get; set; }
+        public double Total
+        {
+            get {return Total; }
+            set
+            {
+                if (value > 0)
+                {
+                    Total = value; 
+                }
+                else
+                {
+                    throw new Exception("You cannot have a negative Total!");
+                }
+            }
+        }
     }
 }
