1b702d942026a360feaf1ba06f45065270d03295
Brian Dawkins <brian.dawkins@reature.net>
2022-01-24 15:09:47 -0500

    cleaning up notes

--- C#/Projects/PokeUI/Program.cs
+++ C#/Projects/PokeUI/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// using PokeModel;
+using PokeUI;
+
+//Console.WriteLine("Hello, World!");
+// Ability ab = new Ability();
+// ab.PP = -1; //Validation is working since can't input a negative value
+
+bool repeat = true;
+IMenu menu = new MainMenu();
+
+while (repeat)
+{
+    Console.Clear();
+    menu.Display();
+    string ans = menu.UserChoice();
+    
+    switch (ans)
+    {
+        case "AddPokemon":
+            menu = new AddPokeMenu();
+            break;
+        case "MainMenu":
+            menu = new MainMenu();
+            break;
+        case "Exit":
+            repeat = false;
+            break;
+        default:
+            Console.WriteLine("Page does not exist!");
+            break;
+    }
+}
